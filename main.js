6d9d3a7b83bad85bdbd8e67910d286b5faa6892f
unknown <khawladahech6@gmail.com>
2024-04-12 16:22:27 +0200

    add the images

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+var you;
+var yourScore = 0;
+var opponent;
+var opponentScore = 0;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+var choices = ["rock", "paper", "scissors"];
 
-setupCounter(document.querySelector('#counter'))
+window.onload = function() {
+    for (let i = 0; i < 3; i++) {
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("cli ck", selectChoice);
+        document.getElementById("choices").append(choice);
+        
+    }
+}
+
+function selectChoice() {
+  you = this.id;
+  document.getElementById("your-choice").src = you + ".png";
+}
+
